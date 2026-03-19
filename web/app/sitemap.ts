a6aa2b80222c059e2475b25817c64ccd79507c8b
Yamil Ayma <yamilaymadev@gmail.com>
2026-03-19 15:35:09 -0500

    (Bug) -  Arreglar el tema de Exportacion Estática para Netlify

--- web/app/sitemap.ts
+++ web/app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next';
 import { getLinks } from '@/lib/data';
 
+export const dynamic = 'force-static';
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = 'https://enlaces-para-desarrolladores.netlify.app';
   const categories = await getLinks();
